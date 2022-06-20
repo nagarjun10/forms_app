637d5a8edd43cbf53b80986246023dc994328952
Nagarjuna devireddy <nagarjunadevireddy007@gmail.com>
2022-06-20 13:11:33 +0530

    api link

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "formsapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
